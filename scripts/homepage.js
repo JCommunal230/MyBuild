667e11d2348e2237ca94b52afc8f7524a7549c08
Jeremy Communal <jeremycommunal@hotmail.fr>
2021-08-26 17:45:38 +0200

    Modification homepage (background, most_played, tooltip, media queries)

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,34 +1,89 @@
-let p = document.querySelectorAll('.posName');
-for (let i = 0; i<p.length; i++){
-	p[i].addEventListener('click', function(){
-		if(p[i].className =='posName active'){
-			p[i].classList.toggle('active')
-			p[i].parentElement.parentElement.nextElementSibling.children[i].classList.toggle('active')
-			p[i].parentElement.parentElement.nextElementSibling.children[i].classList.toggle('hidden')
-			for (let j = 0; j<p.length; j++){
-				p[j].parentElement.parentElement.nextElementSibling.children[j].classList.toggle('hidden')
+
+window.onload=function(){
+	document.querySelector('video').playbackRate = 0.85;/*verify on other browsers*/
+}
+
+let positions = document.querySelectorAll('.positions');
+let tooltip_right = document.querySelectorAll('.tooltiptextright');
+let tooltip_left = document.querySelectorAll('.tooltiptextleft');
+let favorite = document.querySelector('.favorite_champions').children;
+let most_played = document.querySelector('.most_played').children;
+let champions = document.querySelectorAll('.champion');
+
+for (let i = 0; i<positions.length; i++){
+	positions[i].addEventListener('click', function(){
+		if(positions[i].className =='positions active'){
+			positions[i].classList.toggle('active')
+
+			favorite[i].classList.toggle('active')
+			favorite[i].classList.toggle('hidden')
+			for (let j = 0; j<positions.length; j++){
+				favorite[j].classList.toggle('hidden')
 			}
+
+			most_played[i].classList.toggle('active')
+			most_played[i].classList.toggle('hidden')
+
+			for (let j = 0; j<positions.length; j++){
+				most_played[j].classList.toggle('hidden')
+			}
+			for (let k = 0; k<tooltip_right.length; k++){
+				tooltip_right[k].classList.toggle('active')
+			}
+			for (let k = 0; k<tooltip_left.length; k++){
+				tooltip_left[k].classList.toggle('active')
+			}					
 		}
 		else{
-			for (let j = 0; j<p.length; j++){
-					if(p[j].className =='posName active'){
-						p[j].classList.toggle('active')
-						p[j].parentElement.parentElement.nextElementSibling.children[j].classList.toggle('active')
-						p[j].parentElement.parentElement.nextElementSibling.children[j].classList.toggle('hidden')
+			for (let j = 0; j<positions.length; j++){
+					if(positions[j].className =='positions active'){
+						positions[j].classList.toggle('active')
+
+						favorite[j].classList.toggle('active')
+						favorite[j].classList.toggle('hidden')
+
+						most_played[j].classList.toggle('active')
+						most_played[j].classList.toggle('hidden')
+
+						for (let k = 0; k<tooltip_right.length; k++){
+							tooltip_right[k].classList.toggle('active')
+						}
+						for (let k = 0; k<tooltip_left.length; k++){
+							tooltip_left[k].classList.toggle('active')
+						}	
+										
 					}
 					else{
-						if(p[j].parentElement.parentElement.nextElementSibling.children[j].className =='hidden'){
+						if(favorite[j].className =='hidden'){
 							continue
 						}else{
-							p[j].parentElement.parentElement.nextElementSibling.children[j].classList.toggle('hidden')
+							favorite[j].classList.toggle('hidden')
+
+							most_played[j].classList.toggle('hidden')
 						}		
 					}				
 				}		
-			p[i].classList.toggle('active')		
-			p[i].parentElement.parentElement.nextElementSibling.children[i].classList.toggle('hidden')
-			p[i].parentElement.parentElement.nextElementSibling.children[i].classList.toggle('active')
-		}
+			positions[i].classList.toggle('active')		
 
+			favorite[i].classList.toggle('active')
+			favorite[i].classList.toggle('hidden')
+
+			most_played[i].classList.toggle('active')
+			most_played[i].classList.toggle('hidden')
+
+			for (let k = 0; k<tooltip_right.length; k++){
+				tooltip_right[k].classList.toggle('active')
+			}
+			for (let k = 0; k<tooltip_left.length; k++){
+				tooltip_left[k].classList.toggle('active')
+			}
+			
+		}
+		for (let k = 0; k<champions.length; k++){/*trigger the champion animation on each click*/
+				champions[k].style.animation = "none"
+				champions[k].offsetHeight; /* trigger the reflow*/
+ 				champions[k].style.animation = null;
+			}
 	})
 
 }
